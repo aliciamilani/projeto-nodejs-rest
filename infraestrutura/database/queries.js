10fc9d4b2e137404e46ee067796e442dac23b658
Anna Alicia Milani <aliciamilani@users.noreply.github.com>
2020-11-22 21:22:31 -0400

    separando responsabilidades

--- infraestrutura/database/queries.js
+++ infraestrutura/database/queries.js
@@ -3,7 +3,7 @@ const conexao = require('./conexao')
 const executaQuery = (query, parametros = '') => {
     return new Promise((resolve, reject) => {
         conexao.query(query, parametros, (erros, resultados, campos) => {
-            if (erro){
+            if (erros){
                 reject(erros)
             }else{
                 resolve(resultados)
@@ -11,3 +11,5 @@ const executaQuery = (query, parametros = '') => {
         })
     })
 }
+
+module.exports = executaQuery
